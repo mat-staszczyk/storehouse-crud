ef1335a18c4dee30dc7a2a16b8692e5a562715c7
Mateusz <kwiatkiwoknie@gmail.com>
2015-05-10 19:49:59 +0200

    Add main.cpp

---
+++ main.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+
+using namespace std;
+
+int main(void) {
+    cout << "####################" << endl;
+    cout << "# OBSŁUGA MAGAZYNU #" << endl;
+    cout << "####################" << endl << endl;
+    cout << "Prosze wybrac jedna z ponizszych opcji:" << endl << endl;
+    
+    
+    
+    return 0;
+}
